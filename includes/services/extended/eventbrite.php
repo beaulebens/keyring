26616045871c4b9081cd7e580ff30e730fe1947b
Beau Lebens <beau@dentedreality.com.au>
2015-11-28 17:37:52 -0700

    Eventbrite: Bring more in line with WP.com

--- includes/services/extended/eventbrite.php
+++ includes/services/extended/eventbrite.php
@@ -2,7 +2,7 @@
 
 /**
  * Eventbrite service definition for Keyring.
- * https://developer.eventbrite.com/docs/
+ * http://developer.eventbrite.com/
  */
 
 class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
@@ -14,39 +14,53 @@ class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
 	function __construct() {
 		parent::__construct();
 
-		// Enable "basic" UI for entering key/secret
-		if ( !KEYRING__HEADLESS_MODE ) {
-			add_action( 'keyring_eventbrite_manage_ui', array( $this, 'basic_ui' ) );
-			add_filter( 'keyring_eventbrite_basic_ui_intro', array( $this, 'basic_ui_intro' ) );
-		}
-
-		$creds        = $this->get_credentials();
-		$this->key    = $creds['key'];
-		$this->secret = $creds['secret'];
-
-		$this->consumer         = new OAuthConsumer( $this->key, $this->secret, $this->callback_url );
-		$this->signature_method = new OAuthSignatureMethod_HMAC_SHA1;
-
-		$this->authorization_header    = 'Bearer'; // Oh, you
-		$this->authorization_parameter = false;
+		add_filter( 'keyring_' . $this->get_name() . '_request_token_params', array( $this, 'add_connection_referrer' ) );
 
 		$this->set_endpoint( 'authorize', self::OAUTH_BASE . 'authorize', 'GET' );
 		$this->set_endpoint( 'access_token', self::OAUTH_BASE . 'token', 'POST' );
 		$this->set_endpoint( 'self', self::API_BASE . 'users/me/', 'GET' );
+
+		// Enable "basic" UI for entering key/secret
+		if ( ! KEYRING__HEADLESS_MODE ) {
+			add_action( 'keyring_eventbrite_manage_ui', array( $this, 'basic_ui' ) );
+			add_filter( 'keyring_eventbrite_basic_ui_intro', array( $this, 'basic_ui_intro' ) );
+		}
+
+		$creds = $this->get_credentials();
+		$this->app_id  = $creds['app_id'];
+		$this->key     = $creds['key'];
+		$this->secret  = $creds['secret'];
+
+		$this->consumer = new OAuthConsumer( $this->key, $this->secret, $this->callback_url );
+		$this->signature_method = new OAuthSignatureMethod_HMAC_SHA1;
+
+		$this->authorization_header    = 'Bearer';
+		$this->authorization_parameter = false;
+	}
+
+	/**
+	 * Append a referrer to the oAuth request made to Eventbrite, at their request
+	 *
+	 * See http://themedevp2.wordpress.com/2013/12/05/can-we-add-refwpoauth-to/
+	 *
+	 * @param array $params
+	 * @filter keyring_eventbrite_request_token_params
+	 * @return array
+	 */
+	public function add_connection_referrer( $params ) {
+		if ( ! isset( $params['ref'] ) ) {
+			$params['ref'] = 'wpoauth';
+		}
+
+		return $params;
 	}
 
 	function basic_ui_intro() {
-		echo '<p>' . sprintf( __( 'To get started, <a href="%1$s">register an OAuth client on Eventbrite</a>. The most important setting is the <strong>OAuth Redirect URI</strong>, which should be set to <code>%2$s</code>. You can set the other values to whatever you like.', 'keyring' ), 'https://www.eventbrite.com/myaccount/apps/', Keyring_Util::admin_url( 'eventbrite', array( 'action' => 'verify' ) ) ) . '</p>';
-		echo '<p>' . __( "Once you've saved those changes, copy the <strong>CLIENT/APPLICATION KEY</strong> value into the <strong>API Key</strong> field, and the <strong>CLIENT SECRET</strong> value into the <strong>API Secret</strong> field and click save.", 'keyring' ) . '</p>';
+		echo '<p>' . sprintf( __( "To get started, <a href='https://www.eventbrite.com/api/key'>register an OAuth client on Eventbrite</a>. The most important setting is the <strong>OAuth redirect_uri</strong>, which should be set to <code>%s</code>. You can set the other values to whatever you like.", 'keyring' ), esc_url(  Keyring_Util::admin_url( 'eventbrite', array( 'action' => 'verify' ) ) ) ) . '</p>';
+		echo '<p>' . __( "Once you've saved those changes, copy the <strong>APPLICATION KEY</strong> value into the <strong>API Key</strong> field, then click the 'Show' link next to the <strong>OAuth client secret</strong>, copy the value into the <strong>API Secret</strong> field and click save (you don't need an App ID value for Eventbrite).", 'keyring' ) . '</p>';
 	}
 
 	function build_token_meta( $token ) {
-		$meta = array();
-
-		if ( empty( $token['access_token'] ) ) {
-			return $meta;
-		}
-
 		$this->set_token(
 			new Keyring_Access_Token(
 				$this->get_name(),
@@ -54,16 +68,23 @@ class Keyring_Service_Eventbrite extends Keyring_Service_OAuth2 {
 				array()
 			)
 		);
-
 		$response = $this->request( $this->self_url, array( 'method' => $this->self_method ) );
+		$meta = array();
 		if ( !Keyring_Util::is_error( $response ) ) {
-			$meta = array(
-				'user_id' => $response->id,
-				'name'    => $response->name,
-			);
+			if ( isset( $response->emails->email ) ) {
+				$meta['username'] = $response->emails->email;
+			}
+
+			if ( isset( $response->id ) ) {
+				$meta['user_id'] = $response->id;
+			}
+
+			if ( isset( $response->name ) ) {
+				$meta['name'] = $response->name;
+			}
 		}
 
-		return apply_filters( 'keyring_access_token_meta', $meta, 'eventbrite', $token, $response, $this );
+		return apply_filters( 'keyring_access_token_meta', $meta, 'eventbrite', $token, null, $this );
 	}
 
 	function get_display( Keyring_Access_Token $token ) {
