d95869053e6a811b8301268075769a93d0501f46
Beau Lebens <beau@dentedreality.com.au>
2015-11-29 13:02:31 -0700

    Allow optionally not wp_die()ing during a Keyring::error() Bump version number ready for release.

--- keyring.php
+++ keyring.php
@@ -3,7 +3,7 @@
 Plugin Name: Keyring
 Plugin URI: http://dentedreality.com.au/projects/wp-keyring/
 Description: Keyring helps you manage your keys. It provides a generic, very hookable framework for connecting to remote systems and managing your access tokens, username/password combos etc for those services. On its own it doesn't do much, but it enables other plugins to do things that require authorization to act on your behalf.
-Version: 1.6.1
+Version: 1.6.2
 Author: Beau Lebens
 Author URI: http://dentedreality.com.au
 License: GPL v2 or newer <https://www.gnu.org/licenses/gpl.txt>
@@ -26,7 +26,7 @@ define( 'KEYRING__DEBUG_WARN',   2 );
 define( 'KEYRING__DEBUG_ERROR',  3 );
 
 // Indicates Keyring is installed/active so that other plugins can detect it
-define( 'KEYRING__VERSION', '1.6.1' );
+define( 'KEYRING__VERSION', '1.6.2' );
 
 /**
  * Core Keyring class that handles UI and the general flow of requesting access tokens etc
@@ -192,13 +192,16 @@ class Keyring {
 	 * Generic error handler/trigger.
 	 * @param  String $str	Informational message (user-readable)
 	 * @param  array  $info Additional information relating to the error.
+	 * @param  boolean $die If we should immediately die (default) or continue
 	 */
-	static function error( $str, $info = array() ) {
+	static function error( $str, $info = array(), $die = true ) {
 		$keyring = Keyring::init();
 		$keyring->errors[] = $str;
 		do_action( 'keyring_error', $str, $info, isset( $this ) ? $this : null );
-		wp_die( $str, __( 'Keyring Error', 'keyring' ) );
-		exit;
+		if ( $die ) {
+			wp_die( $str, __( 'Keyring Error', 'keyring' ) );
+			exit;
+		}
 	}
 
 	function has_errors() {
